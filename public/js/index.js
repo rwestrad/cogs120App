ec826417650647db0f7e433f21273906b7267083
Ricky Estrada <rickyestrada@Rickys-MacBook-Pro.local>
2019-02-13 23:34:14 -0800

    working flow v2

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,4 @@ window.onload = function() {
   document.getElementById('loginBtn').onclick = function() {
     window.location.href = '/home';
   }
-  document.getElementById('signupBtn').onclick = function() {
-    window.location.href = '/view-applications';
-  }
-  document.getElementById('appName').onclick = function() {
-    window.location.href = '/add-application';
-  }
 }
