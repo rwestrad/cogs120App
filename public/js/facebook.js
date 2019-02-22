f6f75739eb90eee0c806a66ddc77b20d0f33d26a
Jennifer Sandoval <jennifersandoval94@gmail.com>
2019-02-21 16:58:21 -0800

    Added facebook login & nav bar functionality

---
+++ public/js/facebook.js
@@ -0,0 +1,27 @@
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+function statusChangeCallback(response) {
+  console.log('Facebook login status changed.');
+  console.log(response);
+  // The response object is returned with a status field that lets the
+  // app know the current login status of the person.
+  // Full docs on the response object can be found in the documentation
+  // for FB.getLoginStatus().
+  if (response.status === 'connected') {
+    // Logged into your app and Facebook.
+        console.log('Successfully logged in with Facebook');
+         FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
+  }
+}
+
+function fbLogout() {
+    FB.logout(function() {
+        document.getElementById('fbLink').setAttribute("onclick","fbLogin()");
+        document.getElementById('fbLink').innerHTML = '<img src="fblogin.png"/>';
+        document.getElementById('userData').innerHTML = '';
+        document.getElementById('status').innerHTML = 'You have successfully logout from Facebook.';
+    });
