f49ee21bc19d3ba87d2fb6dc0a806f3d6e2fe4da
Jennifer Sandoval <jennifersandoval94@gmail.com>
2019-03-17 16:21:24 -0700

    update company icon depending on first letter

--- routes/add.js
+++ routes/add.js
@@ -1,6 +1,20 @@
 var data = require("../dummy.json");
 
-exports.addCompany = function(request, response) { 
+exports.addCompany = function(request, response) {
+
+  var companyName =  request.query.companyName;
+  var firstLetter = companyName.charAt(0).toUpperCase();
+
+  // RegEx to check for letters only
+  var regex = /[A-Z]/gi;
+  var fLetter;
+
+  //Check if firstLetter is a letter
+  if(regex.test(firstLetter)){
+    fLetter = firstLetter;
+  } else {
+    fLetter = "Default";
+  }  
   var newCompany = {
     "id": data.currentApplications.length + 1,
     "name": request.query.companyName,
@@ -9,7 +23,8 @@ exports.addCompany = function(request, response) { 
     "stage": request.query.stage,
     "website": request.query.website,
     "favorited": false,
-    "deleted": false
+    "deleted": false,
+    "firstLetter":fLetter
   }
   data.currentApplications.push(newCompany);
   response.render('view-applications', data);
